193af24a9fff86b2208300db55b008b49bb4dc70
ArdaSaygan <ardasagii77@gmail.com>
2023-12-23 15:33:42 +0300

    machine

--- machine.py
+++ machine.py
@@ -7,13 +7,35 @@ comm = MPI.Comm.Get_parent()
 size = comm.Get_size()
 rank = comm.Get_rank()
 
-parent_rank = numpy.array(0, dtype='i')
-comm.Bcast([parent_rank, MPI.INT], root=0)
+if rank == 0: # kill this child, for convenience 
+    exit()
 
-print("my rank : ", rank, "parent rank", parent_rank)
+# get main processes id
+main_rank = numpy.array(0, dtype='i')
+comm.Bcast([main_rank, MPI.INT], root=0)
 
-data = comm.recv(source=parent_rank, tag=12)
-print("rank ", rank, " received data ", data)
+# get machine parameters
+machine_init = comm.recv(source=main_rank, tag=1)
+pid, parent_pid, first_operation, feed = machine_init
+print("rank ", rank, machine_init)
+
+# initialize operation list, operation index
+# pid 1 is a special case
+operation_list = []
+op_len = 0
+if (pid == 1):
+    pass
+elif (pid % 2 == 1):
+    operation_list = ["reverse", "trim"]
+    op_len = 2
+else:
+    operation_list = ["enhance", "split", "chop"]
+    op_len = 3
+op_index = 0
+while (pid != 1) and (operation_list[op_index] != first_operation):
+    op_index = (op_index + 1)%op_len
+
+
 
 sleep(2)
 
